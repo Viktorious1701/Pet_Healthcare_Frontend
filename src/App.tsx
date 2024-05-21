fddfee7962eb5e8a88f2303133d36287afc1b813
DangNgocThinhAn <thinhan1701@gmail.com>
2024-05-21 10:45:05 +0700

    my changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BookingForm from './components/BookingForm';
 import './index.css'; // Import Tailwind CSS
 import Calendar from './components/CalendarComponent';
+import Dashboard from './components/Dashboard';
+
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Calendar />} /> {/* Define the route for the next form */}
+        <Route path="/" element={<Dashboard/>} /> {/* Define the route for the next form */}
+        <Route path='/calendar' element={<Calendar/>}></Route>
         <Route path="/book" element={<BookingForm />} />
         
       </Routes>
