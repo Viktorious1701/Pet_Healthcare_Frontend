63042bd8bdd4de9d6e329205b6717d62f7c9e5d2
DangNgocThinhAn <thinhan1701@gmail.com>
2024-05-23 11:27:48 +0700

    my changes

--- src/components/CalendarComponent.tsx
+++ src/components/CalendarComponent.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 import { format } from 'date-fns';
 import { useNavigate } from 'react-router-dom';
 
