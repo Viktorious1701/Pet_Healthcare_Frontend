dc21e1668c2c16bd5c4a5c138f65fd2fd4c71cda
DangNgocThinhAn <thinhan1701@gmail.com>
2024-05-28 19:55:57 +0700

    3. Render.com Update package.json node modules,..

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
-    rollupOptions: {
-      onLog(level, log, handler) {
-        if (log.cause && log.cause.cause === `Can't resolve original location of error.`) {
-          return
-        }
-        handler(level, log)
-      }
-    },
+   
     chunkSizeWarningLimit: 1000, // Adjust the limit as needed
   },
   resolve: {
