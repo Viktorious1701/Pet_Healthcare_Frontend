ea38dabe7690e92622b10e0be868d0579a127c3b
DangNgocThinhAn <thinhan1701@gmail.com>
2024-05-28 20:45:36 +0700

    7. React Router dom update

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,35 @@
-// App.tsx
-import "./index.css";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
-import "./index.css";
-
 import Dashboard from "./pages/Dashboard/Dashboard";
-import Accounts from "./pages/Dashboard/Accounts"; // Import the Accounts component
+import Accounts from "./pages/Dashboard/Accounts";
 import BookingForm from "./components/BookingForm";
 import CalendarComponent from "./components/CalendarComponent";
-import "react-calendar/dist/Calendar.css";
 import Login from "./pages/Login";
-//import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Appointment from "./pages/Dashboard/AppointmentDashboard";
 import Hospitalization from "./pages/Dashboard/Hospitalization";
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<Home/>} />
-      <Route path="/dashboard/*" element={<Dashboard />}>
-        <Route path="appointments" element={<Appointment/>}/>
-        <Route path="accounts" element={<Accounts />} />
-        <Route path="hospitalization" element={<Hospitalization/>} />
-        {/* Nested Accounts route */}
-        {/* Add more nested routes here if needed */}
-      </Route>
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<h1>Services</h1>} />
-      <Route path="/calendar" element={<CalendarComponent />} />
-      <Route path="/book" element={<BookingForm />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/*" element={<h1>404 Not Found</h1>} />
-    </Route>
-  )
-);
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="appointments" element={<Appointment />} />
+          <Route path="accounts" element={<Accounts />} />
+          <Route path="hospitalization" element={<Hospitalization />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<h1>Services</h1>} />
+        <Route path="/calendar" element={<CalendarComponent />} />
+        <Route path="/book" element={<BookingForm />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
