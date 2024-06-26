7e308f01107438978bba0286a42cc3f5dbec42fd
Nguyen Dinh Duong <DuongNDSE183456@fpt.edu.vn>
2024-06-26 08:25:41 +0700

    Styled booking calendar

--- src/components/calendar/CalendarComponent.tsx
+++ src/components/calendar/CalendarComponent.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import '@/../app/globals.css';
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -15,6 +16,10 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ onDateChange }) =
     onDateChange(value, event);
   };
 
+  // Get today's date and set the time to 00:00:00 to ensure the whole day is included
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
   return (
     <div>
       <h2>Select a Date</h2>
@@ -24,6 +29,7 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ onDateChange }) =
           <Calendar
             className="w-full p-2 border border-gray-300 rounded-md bg-[--background]"
             onChange={handleDateChange}
+            minDate={today} // Set the minimum date to today to disable past dates
           />
         </div>
       </div>
