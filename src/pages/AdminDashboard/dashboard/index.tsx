adba08ec2a0825851c1ffdfbb357ff82173c98f3
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-07-23 10:51:52 +0700

    Admin analytics

--- src/pages/AdminDashboard/dashboard/index.tsx
+++ src/pages/AdminDashboard/dashboard/index.tsx
@@ -12,13 +12,20 @@ import { useEffect, useState } from 'react';
 import { PaymentRevenueGet } from '@/Models/Payment';
 import { revenueGetAPI } from '@/Services/PaymentService';
 import { toast } from 'sonner';
-import { IconCalendarMonth, IconCalendarWeek, IconCurrencyDollar, IconDeviceAnalytics } from '@tabler/icons-react';
+import {
+  IconCalendarMonth,
+  IconCalendarWeek,
+  IconCurrencyDollar,
+  IconDeviceAnalytics,
+  IconPercentage
+} from '@tabler/icons-react';
 import { AppointmentGet } from '@/Models/Appointment';
 import { appointmentGetAPI } from '@/Services/AppointmentService';
 import RecentAppointments from './components/recent-appointments';
 import RecentHospitalizations from './components/recent-hospitalizations';
 import { Hospitalization } from '@/Models/Hospitalization';
 import { hospitalizationListAPI } from '@/Services/HospitalizationService';
+import { BarChart, LineChart } from '@mui/x-charts';
 
 export default function Dashboard() {
   const [revenue, setRevenue] = useState<PaymentRevenueGet>();
@@ -167,6 +174,69 @@ export default function Dashboard() {
                 </CardHeader>
                 <CardContent>
                   <RecentAppointments appointments={appointments} />
+                </CardContent>
+              </Card>
+            </div>
+          </TabsContent>
+          <TabsContent value='analytics' className='space-y-4'>
+            <div className='grid gap-4 col-span-1'>
+              <Card>
+                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+                  <CardTitle className='text-sm font-medium'>Revenue Percentage</CardTitle>
+                  <IconPercentage />
+                </CardHeader>
+                <CardContent>
+                  <div className='text-2xl font-bold'>${revenue?.totalRevenue}</div>
+                </CardContent>
+              </Card>
+            </div>
+            <div className='grid grid-cols-1 gap-4 lg:grid-cols-8'>
+              <Card className='col-span-1 lg:col-span-4 h-[58vh] overflow-y-auto'>
+                <CardHeader className='sticky bg-white z-10'>
+                  <CardTitle>Hospitalizations this month</CardTitle>
+                  <CardDescription>
+                    {
+                      hospitalizations.filter(
+                        (hospitalization) =>
+                          new Date(hospitalization.admissionDate).getMonth() === new Date().getMonth() ||
+                          new Date(hospitalization.dischargeDate).getMonth() === new Date().getMonth()
+                      ).length
+                    }{' '}
+                    hospitalizations made this month.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className='pl-2'>
+                  <LineChart
+                    xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+                    series={[
+                      {
+                        data: [2, 5.5, 2, 8.5, 1.5, 5]
+                      }
+                    ]}
+                    width={800}
+                    height={400}
+                  />
+                </CardContent>
+              </Card>
+              <Card className='col-span-1 lg:col-span-4 h-[58vh] overflow-y-auto'>
+                <CardHeader className='sticky top-0 z-10 bg-white'>
+                  <CardTitle>Bookings this month</CardTitle>
+                  <CardDescription>
+                    {
+                      appointments.filter(
+                        (appointment) => new Date(appointment.date).getMonth() === new Date().getMonth()
+                      ).length
+                    }{' '}
+                    appointments booked this month.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <BarChart
+                    xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+                    series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+                    width={800}
+                    height={400}
+                  />
                 </CardContent>
               </Card>
             </div>
